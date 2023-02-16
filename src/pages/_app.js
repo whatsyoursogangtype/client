a3fa506e6ff667e7593c943c9fd42e5381e01a00
wooseok123 <hosmimam@naver.com>
2023-02-16 21:00:51 +0900

    feat : add meta tag

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -10,6 +10,12 @@ export default function App({ Component, pageProps }) {
     <AppLayout>
       <Head>
         <link rel="preload" href="/onLeaf.woff" as="font" type="font/woff" />
+        <title>당신의 서강 유형은?</title>
+        <meta property="og:title" content="당신의 서강 유형은?" key="title" />
+        <meta property="og:url" content="https://sogangtype.com" />
+        <meta property="og:image" content="https://sogangtype.com/likelion-logo.png" />
+        <meta property="og:description" content="당신의 서강 유형은 무엇일까요??" />
+        <meta property="og:site_name" content="whatsyoursogangtype" />
       </Head>
       <Script src="https://developers.kakao.com/sdk/js/kakao.js" />
       <GlobalStyle />
