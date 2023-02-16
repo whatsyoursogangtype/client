43e782dbd8f7e14685747438e09935b2abdaf752
wooseok123 <hosmimam@naver.com>
2023-02-17 02:50:34 +0900

    feat : seo

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,7 @@ import { Text } from "~/@components/atoms/Text";
 import { useRouter } from "next/router";
 import { COLOR_MAIN_RED, COLOR_WHITE_TEXT } from "~/@utils/color";
 import { CharacterImg } from "~/@components/atoms/CharacterImg";
+import Head from "next/head";
 
 export default function Home() {
   const router = useRouter();
@@ -14,6 +15,14 @@ export default function Home() {
 
   return (
     <Flex>
+      <Head>
+        <title>당신의 서강 유형은?</title>
+        <meta property="og:title" content="당신의 서강 유형은?" key="title" />
+        <meta property="og:url" content="https://sogangtype.com" />
+        <meta property="og:image" content="https://sogangtype.com/likelion-logo.png" />
+        <meta property="og:description" content="당신의 서강 유형은 무엇일까요??" />
+        <meta property="og:site_name" content="whatsyoursogangtype" />
+      </Head>
       <Text bigText={true}>
         당신의 서강생활 유형은?
         <br />
@@ -22,11 +31,7 @@ export default function Home() {
 
       <CharacterImg src="likelion.png" width="40%" margin="20px 0 50px" />
       <Button text="테스트 시작하기" onClick={onClick} />
-      <Button
-        text="링크 공유하기"
-        bgColor={COLOR_MAIN_RED}
-        color={COLOR_WHITE_TEXT}
-      />
+      <Button text="링크 공유하기" bgColor={COLOR_MAIN_RED} color={COLOR_WHITE_TEXT} />
 
       <CharacterImg src="likelion-logo.png" width="15%" margin="100px 0" />
     </Flex>
