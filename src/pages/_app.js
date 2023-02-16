6a551a0cc945e08a196d6e4485f801ff302ed919
wooseok123 <hosmimam@naver.com>
2023-02-16 20:50:12 +0900

    feat : minify font

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -9,7 +9,7 @@ export default function App({ Component, pageProps }) {
   return (
     <AppLayout>
       <Head>
-        <link rel="preload" href="/onLeaf.ttf" as="font" type="font/woff" />
+        <link rel="preload" href="/onLeaf.woff" as="font" type="font/woff" />
       </Head>
       <Script src="https://developers.kakao.com/sdk/js/kakao.js" />
       <GlobalStyle />
